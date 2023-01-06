a5100134e73e305febf9a1ff124dbccf2438d032
akberovramil <ramil.akberov96@gmail.com>
2023-01-06 20:27:39 +0300

    First commit

---
+++ src/main/java/ramil/akberov/exapp1/ExApp1Application.java
@@ -0,0 +1,13 @@
+package ramil.akberov.exapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExApp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExApp1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ramil/akberov/exapp1/ExApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ramil.akberov.exapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExApp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
